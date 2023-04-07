61cefc4bfdfe13f4b3d54b7f713e7e98cdabe6bd
Zhe Li <zlhe@microsoft.com>
2023-04-07 19:24:34 +0800

    add anonymous

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import AzureADProvider from "next-auth/providers/azure-ad";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
   providers: [],
@@ -10,6 +11,23 @@ if (process.env.AZURE_AD_CLIENT_ID && process.env.AZURE_AD_CLIENT_SECRET && proc
     clientId: process.env.AZURE_AD_CLIENT_ID,
     clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
     tenantId: process.env.AZURE_AD_TENANT_ID,
+  }));
+}
+
+if (process.env.BASIC_AUTH_USERNAME && process.env.BASIC_AUTH_PASSWOED) {
+  authOptions.providers.push(CredentialsProvider({
+    name: "anonymous",
+    credentials: {
+      username: { label: "Username", type: "text" },
+      password: { label: "Password", type: "password" }
+    },
+    async authorize(credentials, req) {
+      if (credentials?.username === process.env.BASIC_AUTH_USERNAME && credentials?.password === process.env.BASIC_AUTH_PASSWOED) {
+        return { id: "1", name: "Basic Auth", email: "anonymous@lizhe.org" };
+      }
+      
+      return null;
+    }
   }));
 }
 
